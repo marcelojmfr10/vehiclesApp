2bd6c7e4a05fbc2a1c0d9435112a8ad8d9299614
marcelojmfr10 <marcelofuentes_10@hotmail.com>
2021-09-14 21:31:13 -0600

    Navigate to edit/create procedure

--- lib/screens/procedures_screen.dart
+++ lib/screens/procedures_screen.dart
@@ -8,6 +8,7 @@ import 'package:vehicles_app/components/loader_component.dart';
 import 'package:vehicles_app/helpers/constants.dart';
 import 'package:vehicles_app/models/procedure.dart';
 import 'package:vehicles_app/models/token.dart';
+import 'package:vehicles_app/screens/procedure_scree.dart';
 
 class ProceduresScreen extends StatefulWidget {
   final Token token;
@@ -43,7 +44,14 @@ class _ProceduresScreenState extends State<ProceduresScreen> {
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
-        onPressed: () {},
+        onPressed: () {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => ProcedureScreen(
+                      token: widget.token,
+                      procedure: Procedure(id: 0, description: '', price: 0))));
+        },
       ),
     );
   }
@@ -99,7 +107,13 @@ class _ProceduresScreenState extends State<ProceduresScreen> {
       children: _procedures.map((e) {
         return Card(
           child: InkWell(
-            onTap: () {},
+            onTap: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) =>
+                          ProcedureScreen(token: widget.token, procedure: e)));
+            },
             child: Container(
               margin: EdgeInsets.all(10),
               padding: EdgeInsets.all(5),
